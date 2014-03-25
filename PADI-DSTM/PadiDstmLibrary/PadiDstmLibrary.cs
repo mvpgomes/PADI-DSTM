64c91fc9e1a29c88019a59b633c134b4346471db
Marcus Gomes <mvp.gomes1989@gmail.com>
2014-03-25 23:40:31 +0000

    PadiDstmLibrary updated.

--- PADI-DSTM/PadiDstmLibrary/PadiDstmLibrary.cs
+++ PADI-DSTM/PadiDstmLibrary/PadiDstmLibrary.cs
@@ -9,34 +9,31 @@ namespace PadiDstmLibrary
 {
     /**
      * This interface is implemented by the DataServer and its used by the client to create 
-     * and access a PadInt object.
+     * and access a PadInt object, and to realize some tests.
      **/
     public interface IDataServer
     {
         PadInt createPadInt(int uid);
         PadInt accessPadInt(int uid);
-    }
-
-    public interface IMasterServer
-    {
-        string RegDataServer(string url);
-        string GetServerAddr();
-        bool ExistObject(int uid);
-        void ObjCreatedSuccess(string url, int uid);
-    }
-    
-    /**
-     * TODO : Verficar como funciona a biblioteca.
-     **/ 
-    public interface PadiDstmLibrary
-    {
-        bool Init();
         bool TxBegin();
         bool TxCommit();
         bool TxAbort();
-        bool Status();
         bool Fail(string URL);
         bool Freeze(string URL);
         bool Recover(string URL);
     }
+
+    /*
+     * This interface is implemented by the MasterServer and its used by the Application
+     * and the DataServer.
+     **/
+    public interface IMasterServer
+    {
+        bool Init();
+        bool Status();
+        string RegDataServer(string url);
+        string GetServerAddr();
+        bool ExistObject(int uid);
+        void ObjCreatedSuccess(string url, int uid);
+     }
 }
