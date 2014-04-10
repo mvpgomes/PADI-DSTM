38d6529720adf54b2a168295ceb9fabb9fbe3c8f
Marcus Gomes <mvp.gomes1989@gmail.com>
2014-04-10 19:43:34 +0100

    Update Client Form Application.

--- PADI-DSTM/ClientApplicationForm/Form1.cs
+++ PADI-DSTM/ClientApplicationForm/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CommonTypes;
 using PADI_DSTM;
 
 namespace ClientApplicationForm
@@ -25,37 +26,98 @@ namespace ClientApplicationForm
 
         private void CreatePadInt_Click(object sender, EventArgs e)
         {
-            int PadIntID = Convert.ToInt32(CreateTextBox.Text);
-            PadiDstm.CreatePadInt(PadIntID);
+            string PadIntID = CreateTextBox.Text;
+
+            if (PadIntID.Length == 0)
+            {
+                updateLog("Please insert a valid ID.");
+                return;
+            }
+
+            PadInt res = PadiDstm.CreatePadInt(Convert.ToInt32(PadIntID));
+
+            if (res == null)
+            {
+                updateLog("The PadInt with id " + PadIntID + " already exists.");
+            }
         }
 
         private void AccessPadInt_Click(object sender, EventArgs e)
         {
-            int PadIntID = Convert.ToInt32(AccessTextBox.Text);
-            PadiDstm.AccessPadInt(PadIntID);
+            string PadIntID = AccessTextBox.Text;
+
+            if (PadIntID.Length == 0)
+            {
+                updateLog("Please insert a valid ID.");
+                return;
+            }
+
+            PadInt res = PadiDstm.AccessPadInt(Convert.ToInt32(PadIntID));
+
+            if (res == null)
+            {
+                updateLog("The PadInt with id " + PadIntID + " does not exist.");
+            }
         }
 
         private void Fail_Click(object sender, EventArgs e)
         {
             string URL = URLTextBox.Text;
-            PadiDstm.Fail(URL);
+
+            if (URL.Length == 0)
+            {
+                updateLog("Please insert a valid URL.");
+                return;
+            }
+
+            bool res = PadiDstm.Fail(URL);
         }
 
         private void Freeze_Click(object sender, EventArgs e)
         {
             string URL = URLTextBox.Text;
-            PadiDstm.Freeze(URL);
+
+            if (URL.Length == 0)
+            {
+                updateLog("Please insert a valid URL.");
+                return;
+            }
+
+
+            bool res = PadiDstm.Freeze(URL);
+
+            if (res == false)
+            {
+                updateLog("The reuqest could not be completed.");
+            }
         }
 
         private void Recover_Click(object sender, EventArgs e)
         {
             string URL = URLTextBox.Text;
-            PadiDstm.Recover(URL);
+
+            if (URL.Length == 0)
+            {
+                updateLog("Please insert a valid URL.");
+                return;
+            }
+
+            bool res = PadiDstm.Recover(URL);
+
+            if (res == false)
+            {
+                updateLog("The reuqest could not be completed.");
+            }
         }
 
         private void Status_Click(object sender, EventArgs e)
         {
-            PadiDstm.Status();
+            bool res = PadiDstm.Status();
+
+            if (res == false)
+            {
+                updateLog("The reuqest could not be completed.");
+            }
         }
 
         private void TxAbort_Click(object sender, EventArgs e)
@@ -71,6 +133,11 @@ namespace ClientApplicationForm
         private void TxCommit_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void updateLog(string message)
+        {
+            Log.Text += message + "\r\n";
         }
     }
 }
