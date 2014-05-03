267cf718f6e0161c088330fbd746080a46a3ccb7
Marcus Gomes <mvp.gomes1989@gmail.com>
2014-05-03 23:14:55 +0100

    Updated some classes.

--- PADI-DSTM/Server/SayIamAlive.cs
+++ PADI-DSTM/Server/SayIamAlive.cs
@@ -2,11 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Timers;
 using System.Threading.Tasks;
 
 namespace DataServer
 {
     class SayIamAlive
     {
+        private int period;
+        private int delay;
+        private Timer timer;
+
+        /**
+         * SayImAlive single instance  
+         **/
+        private static SayIamAlive instance = null;
+
+        public SayIamAlive()
+        {
+            this.Timer = new Timer();
+        }
+
+        public Timer Timer
+        {
+            get { return timer; }
+            set { timer = value; }
+        }
+
+        public int Delay
+        {
+            get { return delay; }
+            set { delay = value; }
+        }
+
+        public int Period
+        {
+            get { return period; }
+            set { period = value; }
+        }
+
+        public static SayIamAlive getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SayIamAlive();
+            }
+
+            return instance;
+        }
+
+        public void execute()
+        {
+
+        }
+
     }
 }
