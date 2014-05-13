fa263d46c305b8fb3e2ff9327f85a4dc12c49c2a
Miguel Julio <miguel.julio@ist.utl.pt>
2014-05-13 11:33:00 +0100

    Comments added to CommonTypes. PadiDstm in progress

--- PADI-DSTM/CommonTypes/CommonTypes.cs
+++ PADI-DSTM/CommonTypes/CommonTypes.cs
@@ -6,32 +6,56 @@ using System.Threading.Tasks;
 
 namespace CommonTypes
 {
-
+    /// <summary>
+    /// TxException - Exception speficication.
+    /// </summary>
     [Serializable()]
     public class TxException : System.Exception
     {
-        public TxException() : base() { }
-        public TxException(string message) : base(message) { }
-        public TxException(string message, System.Exception inner) : base(message, inner) { }
-
-        // A constructor is needed for serialization when an 
-        // exception propagates from a remoting server to the client.  
+        /**
+         *  TxException constructor.
+         */
+        public TxException() : base() {}
+        /**
+         *  TxException constructor.
+         *  @param message - string message.
+         */
+        public TxException(string message) : base(message) {}
+        /**
+         *  TxException constructor.
+         *  @param message - string message.
+         *  @param inner - System.Exception inner.
+         */
+        public TxException(string message, System.Exception inner) : base(message, inner) {}
+        /**
+         *   TxException - constructor.
+         *   A constructor is needed for serialization when an 
+         *   exception propagates from a remoting server to the client.
+         *   @param info - System.Runtime.Serialization.SerializationInfo info.
+         *   @param context - System.Runtime.Serialization.StreamingContext context.
+         */
         protected TxException(System.Runtime.Serialization.SerializationInfo info,
-            System.Runtime.Serialization.StreamingContext context) { }
+            System.Runtime.Serialization.StreamingContext context){}
     }
 
-    /**
-   * Class that represents a PadInt object that is shared between the distributed system.
-   **/
+    /// <summary>
+    /// PadInt - Class that represents a PadInt object that is shared between the distributed system.
+    /// 
+    /// </summary>
     [Serializable()]
     public class PadInt : MarshalByRefObject
     {
+        /**
+         *  PadInt variables.
+         */
         private int uid;
         private int value;
         private bool read;
         private bool write;
-
-
+        /**
+         *  PadInt constructor.
+         *  @param uid - int that represents PadInt UID.
+         */
         public PadInt(int uid)
         {
             this.uid = uid;
@@ -39,7 +63,11 @@ namespace CommonTypes
             this.read = false;
             this.write = false;
         }
-
+        /**
+         *  PadInt constructor.
+         *  @param uid - int that represents PadInt UID.
+         *  ~@param value - int that represents PadInt value.
+         */
         public PadInt(int uid, int value)
         {
             this.uid = uid;
@@ -47,187 +75,345 @@ namespace CommonTypes
             this.read = false;
             this.write = false;
         }
-
+        /**
+         *  PadInt - wasRead - Method to know if the PadInt was read.
+         *  @return bool.
+         */
         public bool wasRead()
         {
             return read;
         }
-
+        /**
+         *  PadInt - wasWrite - Method to know if the PadInt was wrote.
+         *  @return bool.
+         */
         public bool wasWrite()
         {
             return write;
         }
-
+        /**
+         *  PadInt - getUID - Method to get the UID from the padInt.
+         *  @return an int that represents de PadInt UID.
+         */
         public int getUID()
         {
             return this.uid;
         }
-
         /**
-         *  Method that reads the object PadInt, and return the value of the object.
+         *  PadInt - Read - Method that reads the object PadInt, and return the value of the object.
          *  @throws TxException
-         **/
+         *  @return int that represents PadInt value.
+         */
         public int Read()
         {
             this.read = true;
             return this.value;
         }
-
         /**
-         * Method that writes a value in a PadInt object.
-         * @throws TxException
-         **/
+         *  PadInt - Write - Method that writes a value in a PadInt object.
+         *  @param value - write value into PadInt
+         *  @throws TxException
+         */
         public void Write(int value)
         {
             this.write = true;
-            //local changes
             this.value = value;
         }
-
+        /**
+         *  PadInt - ToString - ToString the padInt content.
+         *  @return string 
+         */
         public override string ToString()
         {
             return "uid: " + this.uid + " val: " + this.value;
         }
     }
 
+    /// <summary>
+    /// PadIntServer - PadIntServer specification.
+    /// This object represents a PadInt on the data server side.
+    /// </summary>
     [Serializable()]
     public class PadIntServer
     {
+        /**
+         *  PadIntServer variables
+         */
         private int uid;
         private int value;
-
+        /**
+         *  PadInt constructor
+         *  @param uid - int that represents PadIntServer uid.
+         */
         public PadIntServer(int uid)
         {
             this.uid = uid;
             this.value = 0;
         }
-
+        /**
+         *  PadIntServer - GetUID - Method that returns the PadIntServer UID.
+         *  Method returns an int.
+         */
         public int GetUID()
         {
             return uid;
         }
-        
+        /**
+         *  PadIntServer - GetValue - Method that returns the PadIntServer value.
+         *  @return an int - the PadIntServer value.
+         */
         public int GetValue()
         {
             return this.value;
         }
-
+        /**
+         *  PadIntServer - SetValue - Method sets the value of value variable.
+         *  @param value - int - the new PadIntServer value.
+         */
         public void SetValue(int value)
         {
-            //local changes
             this.value = value;
         }
-
+        /**
+         *  PadIntServer - ToString - ToString PadIntServer content.
+         *  @return a string.
+         */
         public override string ToString()
         {
             return "uid: " + this.uid + " val: " + this.value;
         }
     }
 
-    /**
-     * Commom Interfaces used by the data servers.
-     **/
+    /// <summary>
+    /// IDataServer - Commom Interface used by the data servers.
+    /// </summary>
     public interface IDataServer
     {
+        /**
+         *  IDataServer - CreateObject.
+         *  @param uid - int object uid.
+         */
         void CreateObject(int uid);
+        /**
+         *  IDataServer - AccessObject.
+         *  @param uid - int object uid.
+         *  @return PadIntServer reference.
+         */ 
         PadIntServer AccessObject(int uid);
+        /**
+         *  IDataServer - Disconnect.
+         *  @return bool - confirmation.
+         */
         bool Disconnect();
+        /**
+         *  IDataServer - DumpState.
+         *  @return bool - confirmation.
+         */
         bool DumpState();
+        /**
+         *  IDataServer - FreezeDataServer.
+         *  @return bool - confirmation.
+         */
         bool FreezeDataServer();
+        /**
+         *  IDataServer - RecoverDataServer.
+         *  @return bool - confirmation.
+         */
         bool RecoverDataServer();
-
-        //Data Servers will implement participant's transaction interface
-        //Call from coordinator to participant to ask whether it can commit a transaction
-        //Participant replies with its vote
+        /**
+         *  IDataServer - CanCommit
+         *  Data Servers will implement participant's transaction interface
+         *  Call from coordinator to participant to ask whether it can commit a transaction
+         *  Participant replies with its vote.
+         *  @param trans - Transaction.
+         *  @return bool - confirmation.
+         */
         bool CanCommit(Transaction trans);
 
-        //Call from coordinator to participant to tell participant to commit its part of a transaction
+        /**
+         *  IDataServer - DoAbort
+         *  Call from coordinator to participant to tell participant to commit its part of a transaction.
+         *  @param padint - TransactionPadInt
+         */
         void DoCommit(TranscationPadInt padint);
 
-        //Call from coordinator to participant to tell participant to abort its part of a transaction
+        /**
+         *  IDataServer - DoCommit
+         *  Call from coordinator to participant to tell participant to abort its part of a transaction.
+         *  @param trans
+         */
         void DoAbort(Transaction trans);
 
-        // Replication Methods
+        /**
+         *  IDataServer - updatePrimaryState
+         *  Replication Methods.
+         *  @param primaryIsAlive - boolean
+         */
         void updatePrimaryState(bool primaryIsAlive);
     }
 
-    /**
-     * Common Interfaces used by the master server
-     **/
+    /// <summary>
+    /// IMasterServer - Common Interfaces used by the master server.
+    /// 
+    /// </summary>
     public interface IMasterServer
     {
+        /**
+         *  IMasterServer - ObjectExists
+         *  @param uid - int uid.
+         *  @return bool - object exists?.
+         */
         bool ObjectExists(int uid);
+        /**
+         *  IMasterServer - ObjCreatedSuccess
+         *  @param url - string url.
+         *  @param uid - int uid.
+         *  @return bool - object created with success?.
+         */
         bool ObjCreatedSuccess(string url, int uid);
-        string GetDataServerAddress();
-        string GetPadIntLocation(int uid);
-        int RegisterDataServer(string url);
+        /**
+         *  IMasterServer - ShowDataServersState
+         *  @return bool - .
+         */
         bool ShowDataServersState();
+        /**
+         *  IMasterServer - GetDataServerAddress
+         *  @return string - Data Server Address.
+         */
+        string GetDataServerAddress();
+        /**
+         *  IMasterServer - GetPadIntLocation
+         *  @param uid - int.
+         *  @return string - PadInt Location.
+         */
+        string GetPadIntLocation(int uid);
+        /**
+         *  IMasterServer - CreateDataServerReplica
+         *  @param dataServerID - data server id.
+         *  @param port - replica port.
+         *  @return string - Replica location.
+         */
         string CreateDataServerReplica(int dataServerID, int port);
+        /**
+         *  IMasterServer - RegisterDataServer
+         *  @param url - string.
+         *  @return int - .
+         */
+        int RegisterDataServer(string url);
+        /**
+         *  IMasterServer - notifyMasterAboutFailure
+         *  Notify Master about the failure.
+         *  @param id - int.
+         *  @param address - string.
+         */
         void notifyMasterAboutFailure(int id, string address);
-        //Master will implement transaction interface
-
-        //Start a new transaction and delivers a unique TID trans
+        /**
+         *  IMasterServer - OpenTransaction
+         *  Start a new transaction and delivers a unique TID trans.
+         *  @return TID
+         */
         TID OpenTransaction();
-
-        //Ends a transaction: (true) a commit return value indicates that the transaction has committed
-        //(false) an abort return value indicates that it has aborted
+        /**
+         *  IMasterServer - CloseTransaction
+         *  Ends a transaction: (true) a commit return value indicates that the transaction has committed
+         *  (false) an abort return value indicates that it has aborted
+         *  @param tid.
+         *  @return bool.
+         */
         bool CloseTransaction(TID tid);
-
-        //Aborts the transaction
+        /**
+         *  IMasterServer - AbortTransaction
+         *  Aborts the transaction.
+         *  @param tid - TID object.
+         */
         void AbortTransaction(TID tid);
-
-        //Informs a coordinator that a new participant has joined the transaction trans
+        /**
+         *  IMasterServer - Join
+         *  Informs a coordinator that a new participant has joined the transaction trans.
+         *  @param trans - TID.
+         *  @param participant - participation identification.
+         */
         void Join(TID trans, int participant);
-
-        //Call from participant to coordinator to confirm that it has committed the transaction
+        /**
+         *  IMasterServer - HaveCommitted
+         *  Call from participant to coordinator to confirm that it has committed the transaction.
+         *  @param tid - TID.
+         *  @param participant - participation identification.
+         */
         void HaveCommitted(TID tid, int participant);
-
-        //Call from participant to coordinator to ask for the decision on a transaction
-        //when it has voted Yes but has still had no reply after some delay
-        //Used to recover from server crash or delayed messages
-
+        /**
+         *  IMasterServer - GetDecision
+         *  Call from participant to coordinator to ask for the decision on a transaction
+         *  when it has voted Yes but has still had no reply after some delay.
+         *  Used to recover from server crash or delayed messages.
+         *  @param tid - TID.
+         *  @return bool.
+         */
         bool GetDecision(TID tid);
-
+        /**
+         *  IMasterServer - AddToTransaction
+         *  Add to transaction.
+         *  @param wasWrite - bool.
+         *  @param value - int.
+         *  @param currentTx - TID.
+         */
         void AddToTransaction(int UID, bool wasWrite, int value, TID currentTx);
     }
 
-    //This is a encapsulation of the Transaction Identifier
+    /// <summary>
+    /// TID - This is a encapsulation of the Transaction Identifier.
+    /// 
+    /// </summary>
     [Serializable()]
     public class TID : IEquatable<TID>
     {
+        /**
+         * TID variables.
+         */ 
         private int id;
         /**
-         * Operations made by client 
+         *  TID constructor
+         *  @param id - int that represents transaction id.
          */
-
         public TID(int id) { 
             this.id = id;
         }
-
+        /**
+         *  TID - GetID
+         *  @return transaction id.
+         */
         public int GetID() { return this.id; }
-
+        /**
+         *  TID - ToString
+         *  @return string - transaction content.
+         */
         public override string ToString()
         {
             return "TID value: " + this.id.ToString();
         }
-        //Comparator for lists
+        /**
+         *  TID - Equals
+         *  Comparator for lists.
+         *  @return bool - equals?.
+         */
         public override bool Equals(object obj)
         {
-            // If parameter is null return false.
             if (obj == null)
             {
                 return false;
             }
-            // If parameter cannot be cast to TID return false.
             TID tid = obj as TID;
             if ((System.Object)tid == null)
             {
                 return false;
             }
-            // Return true if the fields match:
             return tid.GetID() == this.id;
         }
-
+        /**
+         *  TID - Equals
+         *  Comparator for transactions identifier.
+         *  @return bool - equals?.
+         */
         public bool Equals(TID other)
         {
             if (other == null) return false;
@@ -239,14 +425,24 @@ namespace CommonTypes
             return this.id;
         }
     }
-
+    /// <summary>
+    /// TranscationPadInt - Used by master to save transaction operations.
+    /// </summary>
     [Serializable()]
     public struct TranscationPadInt
     {
+        /*
+         * TranscationPadInt variables
+         */
         public int _UID;
         public bool _wasWrite;
         public int _value;
-
+        /*
+         * TranscationPadInt constructor
+         * @param UID - int.
+         * @param wasWrite - bool.
+         * @param value - int.
+         */
         public TranscationPadInt(int UID, bool wasWrite, int value)
         {
             _UID = UID;
@@ -255,41 +451,67 @@ namespace CommonTypes
         }
     }
 
-    //It is a representation of the Transaction
-    //TID must be unique
+    /// <summary>
+    /// Transaction - A representation of the Transaction.
+    /// TID must be unique.
+    /// </summary>
     [Serializable()]
     public class Transaction
     {
+        /*
+         * Transaction variables.
+         */ 
         private TID tid;
         private List<int> partcipants;
         private Dictionary<int, TranscationPadInt> operations;
-
+        /**
+         * Transaction constructor
+         * @param tid - TID.
+         */ 
         public Transaction(TID tid)
         {
             this.tid = tid;
             partcipants = new List<int>();
             operations = new Dictionary<int, TranscationPadInt>();
         }
-
+        /**
+         * Transaction - addOperations
+         * @param operation - TranscationPadInt.
+         */
         public void addOperations(TranscationPadInt operation)
         {
             this.operations.Add(operation._UID, operation);
         }
-
+        /**
+         * Transaction - GetTID
+         * @return TID - from this transaction.
+         */
         public TID GetTID() { return this.tid; }
-
+        /**
+         * Transaction - AddParticipant
+         * @param participant - int participant identifier.
+         */
         public void AddParticipant(int participant)
         {
             this.partcipants.Add(participant);
         }
-
+        /**
+         * Transaction - GetParticipants
+         * @return List<int> - List of participants.
+         */
         public List<int> GetParticipants() { return this.partcipants; }
-
+        /**
+         * Transaction - ToString
+         * @return string - Transaction content.
+         */
         public override string ToString()
         {
             return "Trans with " + tid.ToString();
         }
-
+        /**
+         * Transaction - GetWritePadInts
+         * @return List<TranscationPadInt> - List of TransactionPadInts representing operations made by some client.
+         */
         public List<TranscationPadInt> GetWritePadInts()
         {
             List<TranscationPadInt> writeList = new List<TranscationPadInt>();
@@ -301,9 +523,7 @@ namespace CommonTypes
                     writeList.Add(transPadInt);
                 }
             }
-
             return writeList;
         }
     }
-
-}
+} //CommonTypes
